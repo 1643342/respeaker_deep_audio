98ec30d24e6d0ca4dcf874df09a9ae05c2765a84
Bryan Gonzalez <59040321+1643342@users.noreply.github.com>
2025-11-08 11:18:36 -0500

    Add setup.py for doa_publisher_py package

---
+++ src/doa_publisher_py/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+package_name = 'doa_publisher_py'
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='Publish DOA from ReSpeaker via USB',
+    license='Apache-2.0',
+    entry_points={'console_scripts': ['doa_node = doa_publisher_py.doa_node:main']},
+)
